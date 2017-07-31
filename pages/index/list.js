f7a28aad6d8816e10bf24c3954528bb807d039d1
wang.zhisheng <wang.zhisheng@jyall.com>
2017-07-31 17:19:37 +0800

    list

--- pages/index/list.js
+++ pages/index/list.js
@@ -6,4 +6,17 @@ Page(observer(Object.assign({}, Toast, {
   props: {
     searchInfo: require('../../stores/search.js').default,
   },
+  onLoad: function(){
+    wx.setNavigationBarTitle({
+      title: "北京-太原"
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#22ca92',
+      animation: {
+        duration: 400,
+        timingFunc: 'easeIn'
+      }
+    })
+  }
 })))
